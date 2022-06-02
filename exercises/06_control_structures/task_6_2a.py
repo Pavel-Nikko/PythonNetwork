0991976b06eac3b23740bdc752d6e3532f26d5bd
admiral.1990@mail.ru <P@ssw0rd78563211236587>
2022-06-02 16:48:12 +0300

    task 6.2a done

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,31 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input("Введите IP адрес в формате x.x.x.x :")
+
+octet = ip.split(".")
+good_ip = True
+if len(ip.split(".")) != 4:
+    good_ip = False
+else:
+    for octets in octet:
+        if not (octets.isdigit() and int(octets) in range(256)):
+            good_ip = False
+
+if not good_ip:
+    print("неправильный IP")
+
+else:
+    if ip == "0.0.0.0":
+        print("unassigned")
+    elif  int(ip.split(".")[0]) >= 0 and int(ip.split(".")[0]) <= 223:
+        print("unicast")
+    elif int(ip.split(".")[0]) >= 224 and int(ip.split(".")[0]) <= 239:
+        print("multicast")
+    elif ip == "255.255.255.255":
+        print("local broadcast")
+    else:
+        print("unused")
+
+
+
