453419ddcd6b700eea981cbc18847c0bd2f18603
admiral.1990@mail.ru <P@ssw0rd78563211236587>
2022-06-22 13:35:41 +0300

    task 7.1 done

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -13,4 +13,22 @@ Outbound Interface    FastEthernet0/0
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
+
+param = ['Prefix','AD/Metric','Next-Hop','Last update','Outbound Interface']
+with open('ospf.txt', 'r') as f:
+    for line in f:
+        for paramm in param:
+            print(f'{paramm:<25}{line.split()[1]}')
 """
+param = ['Prefix','AD/Metric','Next-Hop','Last update','Outbound Interface']
+
+with open('ospf.txt', 'r') as f:
+    for line in f:
+        value = line.replace("[", " ").replace("]", " ").replace(",", " ").split()
+        print(("{:25}{}\n"*5).format(param[0],value[1],
+                                   param[1],value[2],
+                                   param[2],value[4],
+                                   param[3],value[5],
+                                   param[4],value[6]
+                                   ))
+
