4525d59d9e59de2acc6d173a65216253b026b58a
admiral.1990@mail.ru <P@ssw0rd78563211236587>
2022-06-23 17:33:40 +0300

    task 7.2a done

--- exercises/07_files/task_7_2a.py
+++ exercises/07_files/task_7_2a.py
@@ -8,7 +8,7 @@
 в которых содержатся слова из списка ignore.
 
 При этом скрипт также не должен выводить строки, которые начинаются на !.
-
+pass
 Проверить работу скрипта на конфигурационном файле config_sw1.txt.
 Имя файла передается как аргумент скрипту.
 
@@ -17,3 +17,21 @@
 """
 
 ignore = ["duplex", "alias", "configuration"]
+file = "config_sw1.txt"
+
+
+with open(file) as f:
+    for line in f:
+        words = line.split()
+        words_intersect = set(words).intersection(set(ignore))
+        if not line.startswith("!") and not words_intersect:
+            print(line.rstrip())
+
+
+
+
+
+
+
+
+
