7c1e28ff9526547bca1ca3876f521d544118b1a1
admiral.1990@mail.ru <P@ssw0rd78563211236587>
2022-06-27 17:52:31 +0300

    task 7.3b done

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -17,3 +17,18 @@ Enter VLAN number: 10
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+inter1 = []
+b = input("Введите влан:")
+with open("CAM_table.txt") as src:
+    for line in src:
+        if len(line.split()) == 4 and line.split()[0].isdigit() and line.split()[0] ==b:
+            inter = "".join(line).split()
+            vlan, mac, type, port = inter
+            inter1.append([int(vlan), mac, port])
+
+
+    for vlan, mac, port in sorted(inter1):
+        print(f'{int(vlan):<8}{mac:<20}{port}')
+
+
